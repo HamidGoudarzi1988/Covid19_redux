4f535b8425b1a49e0cd1dca147daaa15890208e8
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2020-08-12 13:19:28 -0400

    Updates

---
+++ precache-manifest.6870e96222bd3d1c9cbcfbf0218633e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432e6774f79728eb2f84fd6ee4096d74",
+    "url": "/Covid19_redux/index.html"
+  },
+  {
+    "revision": "788a0dea30526120fd83",
+    "url": "/Covid19_redux/static/css/main.efa333c3.chunk.css"
+  },
+  {
+    "revision": "20388352134616ad1912",
+    "url": "/Covid19_redux/static/js/2.2d50fae1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19_redux/static/js/2.2d50fae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788a0dea30526120fd83",
+    "url": "/Covid19_redux/static/js/main.00cb7379.chunk.js"
+  },
+  {
+    "revision": "9f6093df0d14c0bb3bed",
+    "url": "/Covid19_redux/static/js/runtime-main.d3285064.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19_redux/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "43707003ae49ee0b845cb356267d3bfa",
+    "url": "/Covid19_redux/static/media/image2.43707003.jpg"
+  }
+]);
